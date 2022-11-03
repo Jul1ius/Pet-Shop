57611d6a61bc9bc4345e7227aaab6ef19331bf84
nanji <nanji@DESKTOP-4A8MIDK>
2022-11-03 22:07:00 +0300

    Add project files.

---
+++ Pet Shop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogpack = double.Parse(Console.ReadLine());
+            double dogpackPrice = dogpack * 2.50;
+
+            double catpack = double.Parse(Console.ReadLine());
+            double catpackPrice = catpack * 4;
+
+            double tottal = dogpackPrice + catpackPrice;
+
+            Console.WriteLine($"{tottal} lv.");
+        }
+    }
+}
